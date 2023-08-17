606e9fa1a35ec59248301a24bfffd6d6a82bcaa3
arielsonSantos <arielsonelias@gmail.com>
2023-08-17 02:40:42 -0300

    Implementing Dumpster/Dumpster Type editing/deletion

--- src/main/java/br/com/arielsonsantos/drms/controllers/DumpsterController.java
+++ src/main/java/br/com/arielsonsantos/drms/controllers/DumpsterController.java
@@ -5,14 +5,17 @@ import java.util.Optional;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.arielsonsantos.drms.entities.Dumpster;
+import br.com.arielsonsantos.drms.exceptions.EntityNotFoundException;
 import br.com.arielsonsantos.drms.services.DumpsterService;
 
 @RestController
@@ -32,7 +35,7 @@ public class DumpsterController {
 	}
 	
 	@GetMapping("/{id}")
-	public Optional<Dumpster> findAll(@PathVariable Long id) {
+	public Optional<Dumpster> findById(@PathVariable Long id) {
 		return dumpsterService.findById(id);
 	}
 	
@@ -41,5 +44,23 @@ public class DumpsterController {
 		dumpsterService.save(dumpster);
 		return ResponseEntity.status(HttpStatus.CREATED).body(dumpster);
 	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<Dumpster> update(@PathVariable Long id, @RequestBody Dumpster newDumpster) {
+		final Dumpster dumpster = dumpsterService.findById(id).orElseThrow(() -> new EntityNotFoundException("Caçamba", id));
+	
+		dumpster.setIdentifier(newDumpster.getIdentifier());
+		dumpster.setType(newDumpster.getType());
+		
+		dumpsterService.save(dumpster);
+		return ResponseEntity.status(HttpStatus.CREATED).body(newDumpster);
+	}
 
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Object> deleteById(@PathVariable Long id) {
+		final Dumpster dumpster = dumpsterService.findById(id).orElseThrow(() -> new EntityNotFoundException("Caçamba", id));
+		
+		dumpsterService.delete(dumpster);
+		return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+	}
 }
